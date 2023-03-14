956545637183943e21893a462ce18fe1049f20ba
DavidSaruni <sarunidavid11126@gmail.com>
2023-03-14 18:13:14 +0300

    user authentication

--- contacts/urls.py
+++ contacts/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('login/', views.loginPage, name='login'),
+    path('logout/', views.logoutUser, name='logout'),
     path('register/', views.registerPage, name='register'),
     path('', views.home, name='home'),
 
--- contacts/views.py
+++ contacts/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 from .models import *
 from .forms import CreateUserForm
@@ -12,37 +13,47 @@ from .forms import CreateUserForm
 
 # Create your views here.
 def registerPage(request):
-    form = CreateUserForm()
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        form = CreateUserForm()
 
-    #let us process our form
-    if request.method == 'POST':
-        form = CreateUserForm(request.POST) #pass in the data
-        if form.is_valid(): #save the data
-            form.save()
-            user = form.cleaned_data.get('username')
-            messages.success(request, 'Account for ' + user + ' was created')
-            return redirect('login')
+        #let us process our form
+        if request.method == 'POST':
+            form = CreateUserForm(request.POST) #pass in the data
+            if form.is_valid(): #save the data
+                form.save()
+                user = form.cleaned_data.get('username')
+                messages.success(request, 'Account for ' + user + ' was created')
+                return redirect('login')
 
-    context = {'form': form}
-    return render(request, 'contacts/register.html', context)
+        context = {'form': form}
+        return render(request, 'contacts/register.html', context)
 
 def loginPage(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
 
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
+            user = authenticate(request, username=username, password=password)
 
-        user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                messages.info(request, 'Username or Password is incorrect')
 
-        if user is not None:
-            login(request, user)
-            return redirect('home')
-        else:
-            messages.info(request, 'Username or Password is incorrect')
+        context = {}
+        return render(request, 'contacts/login.html', context)
 
-    context = {}
-    return render(request, 'contacts/login.html', context)
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
 
+@login_required(login_url='login')
 def home(request):
     context = {}
-    return render(request, 'contacts/contact_list.html', context)
+    return render(request, 'contacts/contact_list.html', context) 
