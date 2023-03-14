a53f06f5b458694a1f67e44b9d19f900655dfc4a
DavidSaruni <sarunidavid11126@gmail.com>
2023-03-14 11:10:25 +0300

    login and register

---
+++ contacts/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('login/', views.loginPage, name='login'),
+    path('register/', views.registerPage, name='register'),
+    path('', views.home, name='home'),
+
+]
---
+++ contacts/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render, redirect
+from django.forms import inlineformset_factory
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+
+from django.contrib.auth import authenticate, login, logout
+
+from .models import *
+from .forms import CreateUserForm
+
+
+
+# Create your views here.
+def registerPage(request):
+    form = CreateUserForm()
+
+    #let us process our form
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST) #pass in the data
+        if form.is_valid(): #save the data
+            form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, 'Account for ' + user + ' was created')
+            return redirect('login')
+
+    context = {'form': form}
+    return render(request, 'contacts/register.html', context)
+
+def loginPage(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.info(request, 'Username or Password is incorrect')
+
+    context = {}
+    return render(request, 'contacts/login.html', context)
+
+def home(request):
+    context = {}
+    return render(request, 'contacts/contact_list.html', context)
